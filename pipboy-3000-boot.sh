a9008b6412aec0ef3cc3b3c64d00e569c061c574
kleo <kbeflo@gmail.com>
2021-08-30 04:05:00 +0800

    Fix text blink

--- pipboy-3000-boot.sh
+++ pipboy-3000-boot.sh
@@ -6,6 +6,6 @@ cat scripts/version | pv -q -L 50
 sleep 2
 clear
 cat scripts/vaultboy | pv -q -L 200
-echo "\033[5m          INITIATING...\033[0m"
+echo -e "\033[5m          INITIATING...\033[0m"
 sleep 5
 clear
